d34fa89d30789763efb92606aa7a3a20531d033c
ilan.klein <ilan.klein@similarweb.co.il>
2021-07-25 16:21:14 +0300

    more work

--- src/components/GraphConfigPanel.tsx
+++ src/components/GraphConfigPanel.tsx
@@ -10,43 +10,48 @@ const Container = styled.div`
 `;
 
 export const GraphConfigPanel = () => {
-    const [isOpened, setIsOpened] = useState(true);
+    const [isOpened, setIsOpened] = useState({
+        title: true,
+        legend: true,
+        graph: true,
+        threeD: true,
+    });
 
     function onTransitionEnd(evt: TransitionEvent) {
         console.log("transition ended", evt);
     }
 
-    function handleOpen() {
-        setIsOpened(!isOpened);
+    function handleOpen(evt: any) {
+        console.log(evt);
+        // setIsOpened(!isOpened);
     }
     return (
         <Container>
-            <CollapsibleToggle isActive={isOpened} text={"Title"} onClick={handleOpen} />
-            <Collapsible isActive={isOpened} onTransitionEnd={onTransitionEnd}>
+            <CollapsibleToggle isActive={isOpened.title} text={"Title"} onClick={handleOpen} />
+            <Collapsible isActive={isOpened.title} onTransitionEnd={onTransitionEnd}>
                 <ul>
-                    <li>test1</li>
-                    <li>test2</li>
-                    <li>test3</li>
+                    <li>text</li>
+                    <li>period</li>
+                    <li>counting</li>
+                    <li>I cannot read the english!!</li>
+                    <li>toolitp</li>
                 </ul>
             </Collapsible>
-            <CollapsibleToggle isActive={isOpened} text={"Legend"} onClick={handleOpen} />
-            <Collapsible isActive={isOpened} onTransitionEnd={onTransitionEnd}>
+            <CollapsibleToggle isActive={isOpened.legend} text={"Legend"} onClick={handleOpen} />
+            <Collapsible isActive={isOpened.legend} onTransitionEnd={onTransitionEnd}>
                 <ul>
-                    <li>test1</li>
-                    <li>test2</li>
-                    <li>test3</li>
+                    <li>Has Icon</li>
                 </ul>
             </Collapsible>
-            <CollapsibleToggle isActive={isOpened} text={"Graph"} onClick={handleOpen} />
-            <Collapsible isActive={isOpened} onTransitionEnd={onTransitionEnd}>
+            <CollapsibleToggle isActive={isOpened.graph} text={"Graph"} onClick={handleOpen} />
+            <Collapsible isActive={isOpened.graph} onTransitionEnd={onTransitionEnd}>
                 <ul>
-                    <li>test1</li>
-                    <li>test2</li>
-                    <li>test3</li>
+                    <li>do do do</li>
+                    <li>gap</li>
                 </ul>
             </Collapsible>
-            <CollapsibleToggle isActive={isOpened} text={"3D"} onClick={handleOpen} />
-            <Collapsible isActive={isOpened} onTransitionEnd={onTransitionEnd}>
+            <CollapsibleToggle isActive={isOpened.threeD} text={"3D"} onClick={handleOpen} />
+            <Collapsible isActive={isOpened.threeD} onTransitionEnd={onTransitionEnd}>
                 <ul>
                     <li>test1</li>
                     <li>test2</li>
