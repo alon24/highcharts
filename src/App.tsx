2234dba8a84c9bc23f539b18e1c9d94d7da72d18
Gal Zohar <gal.zohar@similarweb.com>
2021-07-26 02:20:18 +0300

    done working on props

--- src/App.tsx
+++ src/App.tsx
@@ -9,52 +9,52 @@ import { GraphPreview } from "./components/GraphPreview/GraphPreview";
 import { TopBar } from "./components/TopBar/TopBar";
 
 export const App: React.FC = () => {
+    /**
+     * Top Panel Props
+     */
     const [selectedGraphType, setSelectedGraphType] = useState<GraphType>("Pie");
     const [selectedTab, setSelectedTab] = useState<TabType>("Design");
     const [is3DGraph, setIs3DGraph] = useState(false);
     const [dataSet, setDataSet] = useState<DataSet>("Gender");
     const [seriesCount, setSeriesCount] = useState<number>(2);
 
+    /**
+     * Section Toggles
+     */
     const [isHeaderSectionOpen, setIsHeaderSectionOpen] = useState(true);
     const [isLegendSectionOpen, setIsLegendSectionOpen] = useState(false);
     const [isGraphSectionOpen, setIsGraphSectionOpen] = useState(false);
     const [isTooltipSectionOpen, setIsTooltipSectionOpen] = useState(false);
     const [is3DSectionOpen, setIs3DSectionOpen] = useState(false);
 
-    const handleSelectGraphType = useCallback(
-        (type: GraphType) => {
-            setSelectedGraphType(type);
-        },
-        [selectedGraphType],
-    );
+    /**
+     * Header Section Props
+     */
+    const [chartTitle, setChartTitle] = useState("Chart Title");
+    const [chartTooltip, setChartTooltip] = useState<string>(null);
+    const [hasPeriod, setHasPeriod] = useState(true);
+    const [hasCountry, setHasCountry] = useState(true);
+    const [hasTrafficSource, setHasTrafficSource] = useState(true);
 
-    const handleSelectTab = useCallback(
-        (type: TabType) => {
-            setSelectedTab(type);
-        },
-        [selectedGraphType],
-    );
+    /**
+     * Legend Section Props
+     */
+    const [isIconLegend, setIsIconLegend] = useState(false);
 
-    const handleSetIs3DGraph = useCallback(
-        (is3D: boolean) => {
-            setIs3DGraph(is3D);
-        },
-        [setIs3DGraph],
-    );
+    /**
+     * Graph Section Props
+     */
+    // Sets the distance between pieChart pieces
+    const [pieChartSliceGap, setPieChartSliceGap] = useState(3);
+    // If not piechart - then its doughnut
+    const [isPieChart, setIsPieChart] = useState(false);
 
-    const handleSelectDataSet = useCallback(
-        (dataSet: DataSet) => {
-            setDataSet(dataSet);
-        },
-        [setDataSet],
-    );
-
-    const handleSetSeriesCount = useCallback(
-        (count: number) => {
-            setSeriesCount(count);
-        },
-        [setSeriesCount],
-    );
+    /**
+     * 3D Section Props
+     */
+    const [alphaAngle, setAlphaAngle] = useState(0);
+    const [betaAngle, setBetaAngle] = useState(0);
+    const [depth, setDepth] = useState<number>(null);
 
     const context = {
         panelTab: selectedTab,
@@ -62,23 +62,51 @@ export const App: React.FC = () => {
         is3DGraph: is3DGraph,
         dataSet: dataSet,
         seriesCount: seriesCount,
-        setPanelTab: handleSelectTab,
-        setGraphType: handleSelectGraphType,
-        setIs3DGraph: handleSetIs3DGraph,
-        setDataSet: handleSelectDataSet,
-        setSeriesCount: handleSetSeriesCount,
+        setPanelTab: (type: TabType) => setSelectedTab(type),
+        setGraphType: (type: GraphType) => setSelectedGraphType(type),
+        setIs3DGraph: (is3D: boolean) => setIs3DGraph(is3D),
+        setDataSet: (dataSet: DataSet) => setDataSet(dataSet),
+        setSeriesCount: (count: number) => setSeriesCount(count),
 
         isHeaderSectionOpen: isHeaderSectionOpen,
         isLegendSectionOpen: isLegendSectionOpen,
         isGraphSectionOpen: isGraphSectionOpen,
         isTooltipSectionOpen: isTooltipSectionOpen,
         is3DSectionOpen: is3DSectionOpen,
-
         setIsHeaderSectionOpen: (isOpen) => setIsHeaderSectionOpen(isOpen),
         setIsLegendSectionOpen: (isOpen) => setIsLegendSectionOpen(isOpen),
         setIsGraphSectionOpen: (isOpen) => setIsGraphSectionOpen(isOpen),
         setIsTooltipSectionOpen: (isOpen) => setIsTooltipSectionOpen(isOpen),
         setIs3DSectionOpen: (isOpen) => setIs3DSectionOpen(isOpen),
+
+        chartTitle: chartTitle,
+        chartTooltip: chartTooltip,
+        hasPeriod: hasPeriod,
+        hasCountry: hasCountry,
+        hasTrafficSource: hasTrafficSource,
+        setChartTitle: (title: string) => setChartTitle(title),
+        setTooltip: (tooltip: string) => setChartTooltip(tooltip),
+        setHasPeriod: (hasPeriod: boolean) => setHasPeriod(hasPeriod),
+        setHasCountry: (hasCountry: boolean) => setHasCountry(hasCountry),
+        setHasTrafficSource: (hasTrafficSource: boolean) => setHasTrafficSource(hasTrafficSource),
+
+        isIconLegend: isIconLegend,
+        setIsIconLegend: (isIcon: boolean) => setIsIconLegend(isIcon),
+
+        pieChartSliceGap: pieChartSliceGap,
+        setPieChartSliceGap: (gap: number) => setPieChartSliceGap(gap),
+
+        isPieChart: isPieChart,
+        setIsPieChart: (isPieChart: boolean) => setIsPieChart(isPieChart),
+
+        alphaAngle: alphaAngle,
+        setAlphaAngle: (alpha: number) => setAlphaAngle(alpha),
+
+        betaAngle: betaAngle,
+        setBetaAngle: (beta: number) => setBetaAngle(beta),
+
+        depth: depth,
+        setDepth: (depth: number) => setDepth(depth),
     };
 
     return (
--- src/AppContext/AppContext.tsx
+++ src/AppContext/AppContext.tsx
@@ -4,6 +4,9 @@ import { DataSet, GraphType, TabType } from "../AppTypes";
 const noopFunc = () => void 0;
 
 export interface IAppContext {
+    /**
+     * Control Panel Top Props
+     */
     panelTab: TabType;
     graphType: GraphType;
     is3DGraph: boolean;
@@ -15,17 +18,62 @@ export interface IAppContext {
     setDataSet: (dataSet: DataSet) => void;
     setSeriesCount: (count: number) => void;
 
+    /**
+     * Section Toggles
+     */
     isHeaderSectionOpen: boolean;
     isLegendSectionOpen: boolean;
     isGraphSectionOpen: boolean;
     isTooltipSectionOpen: boolean;
     is3DSectionOpen: boolean;
-
     setIsHeaderSectionOpen: (isOpen: boolean) => void;
     setIsLegendSectionOpen: (isOpen: boolean) => void;
     setIsGraphSectionOpen: (isOpen: boolean) => void;
     setIsTooltipSectionOpen: (isOpen: boolean) => void;
     setIs3DSectionOpen: (isOpen: boolean) => void;
+
+    /**
+     * Header Section Props
+     */
+    chartTitle: string;
+    chartTooltip?: string;
+    hasPeriod: boolean;
+    hasCountry: boolean;
+    hasTrafficSource: boolean;
+    setChartTitle: (title: string) => void;
+    setTooltip: (tooltip: string) => void;
+    setHasPeriod: (hasPeriod: boolean) => void;
+    setHasCountry: (hasCountry: boolean) => void;
+    setHasTrafficSource: (hasTrafficSource: boolean) => void;
+
+    /**
+     * Legend Section Props
+     */
+    isIconLegend: boolean;
+    setIsIconLegend: (isIcon: boolean) => void;
+
+    /**
+     * Graph Section Props
+     */
+    // Sets the distance between pieChart pieces
+    pieChartSliceGap: number;
+    setPieChartSliceGap: (gap: number) => void;
+
+    // Sets if the pieChart should be a pie or dougnut
+    isPieChart: boolean;
+    setIsPieChart: (isPieChart: boolean) => void;
+
+    /**
+     * 3D Section Props
+     */
+    alphaAngle: number;
+    setAlphaAngle: (alpha: number) => void;
+
+    betaAngle: number;
+    setBetaAngle: (beta: number) => void;
+
+    depth?: number;
+    setDepth: (depth: number) => void;
 }
 
 export const AppContext = React.createContext<IAppContext>({
@@ -45,10 +93,35 @@ export const AppContext = React.createContext<IAppContext>({
     isGraphSectionOpen: false,
     isTooltipSectionOpen: false,
     is3DSectionOpen: false,
-
     setIsHeaderSectionOpen: noopFunc,
     setIsLegendSectionOpen: noopFunc,
     setIsGraphSectionOpen: noopFunc,
     setIsTooltipSectionOpen: noopFunc,
     setIs3DSectionOpen: noopFunc,
+
+    chartTitle: "Chart Title",
+    hasPeriod: true,
+    hasCountry: true,
+    hasTrafficSource: true,
+    setChartTitle: noopFunc,
+    setTooltip: noopFunc,
+    setHasPeriod: noopFunc,
+    setHasCountry: noopFunc,
+    setHasTrafficSource: noopFunc,
+
+    isIconLegend: false,
+    setIsIconLegend: noopFunc,
+
+    pieChartSliceGap: 2,
+    setPieChartSliceGap: noopFunc,
+
+    isPieChart: false,
+    setIsPieChart: noopFunc,
+
+    alphaAngle: 0,
+    setAlphaAngle: noopFunc,
+
+    betaAngle: 0,
+    setBetaAngle: noopFunc,
+    setDepth: noopFunc,
 });
